f6225b2e6d05a9796dd11172dfff7bcee4734684
0LL1 <o.a.h.rautiainen@gmail.com>
2019-04-22 20:16:27 +0300

    Refactor MainTasks to hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { GlobalStyle, StyledApp } from './styles'
 import Stopwatch from './Stopwatch'
 import Timer from './Timer'
 import Barca from './Barca'
 import MainTasks from './MainTasks'
 
-class App extends Component {
-  render() {
-    return (
-      <StyledApp>
-        <GlobalStyle />
-        <Stopwatch />
-        <Timer />
-        <Barca />
-        <MainTasks />
-      </StyledApp>
-    )
-  }
+const App = () => {
+  return (
+    <StyledApp>
+      <GlobalStyle />
+      <Stopwatch />
+      <Timer />
+      <Barca />
+      <MainTasks />
+    </StyledApp>
+  )
 }
 
 export default App
--- src/MainTasks.js
+++ src/MainTasks.js
@@ -1,82 +1,58 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import uuidv4 from 'uuid/v4'
 import { MdDone } from 'react-icons/md'
 import { StyledMainTasks, Title, TaskInput, TaskList, Task } from './styles'
 
-class MainTasks extends Component {
-  state = {
-    tasks: [],
-    input: ''
+const MainTasks = () => {
+  const initialTasks = () => JSON.parse(localStorage.getItem('tasks')) || []
+  const [tasks, setTasks] = useState(initialTasks)
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks])
+
+  const setTask = event => {
+    setInput(event.target.value)
   }
 
-  componentDidMount() {
-    const savedTasks = JSON.parse(localStorage.getItem('tasks'))
-    savedTasks && this.setState({ tasks: savedTasks })
-  }
-
-  setTask = event => {
-    this.setState({ input: event.target.value })
-  }
-
-  addTask = () => {
-    if (this.state.tasks.length < 3) {
-      this.setState(
-        prevState => {
-          return {
-            tasks: [
-              ...prevState.tasks,
-              { task: this.state.input, id: uuidv4() }
-            ],
-            input: ''
-          }
-        },
-        () => {
-          localStorage.setItem('tasks', JSON.stringify(this.state.tasks))
-        }
-      )
+  const addTask = () => {
+    if (tasks.length < 3) {
+      const newTasks = [...tasks, { task: input, id: uuidv4() }]
+      setTasks(newTasks)
+      setInput('')
     } else return
   }
 
-  deleteTask = id => {
-    this.setState(
-      prevState => {
-        return { tasks: prevState.tasks.filter(task => task.id !== id) }
-      },
-      () => {
-        localStorage.setItem('tasks', JSON.stringify(this.state.tasks))
-      }
-    )
+  const deleteTask = id => {
+    setTasks(prevState => prevState.filter(task => task.id !== id))
   }
 
-  render() {
-    const { tasks, input } = this.state
-
-    const taskList = tasks.map(task => {
-      return (
-        <Task key={task.id}>
-          <MdDone className="icon" onClick={() => this.deleteTask(task.id)} />
-          {task.task}
-        </Task>
-      )
-    })
-
+  const taskList = tasks.map(task => {
     return (
-      <StyledMainTasks>
-        <Title>main tasks of today</Title>
-        <label>
-          {tasks.length >= 3 ? 'no more tasks!' : 'new task'}
-          <TaskInput
-            onChange={this.setTask}
-            onKeyDown={e => e.key === 'Enter' && input && this.addTask()}
-            value={input}
-            hidden={tasks.length >= 3}
-            maxLength="50"
-          />
-        </label>
-        <TaskList>{taskList}</TaskList>
-      </StyledMainTasks>
+      <Task key={task.id}>
+        <MdDone className="icon" onClick={() => deleteTask(task.id)} />
+        {task.task}
+      </Task>
     )
-  }
+  })
+
+  return (
+    <StyledMainTasks>
+      <Title>main tasks of today</Title>
+      <label>
+        {tasks.length >= 3 ? 'no more tasks!' : 'new task'}
+        <TaskInput
+          onChange={setTask}
+          onKeyDown={e => e.key === 'Enter' && input && addTask()}
+          value={input}
+          hidden={tasks.length >= 3}
+          maxLength="50"
+        />
+      </label>
+      <TaskList>{taskList}</TaskList>
+    </StyledMainTasks>
+  )
 }
 
 export default MainTasks
